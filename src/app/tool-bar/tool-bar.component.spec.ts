c05da5bea4158a10a33470c82a0752885dc0e09a
David Ildefonso <david_lion_ig@outlook.com.pe>
2022-01-30 13:57:53 -0500

    added top bar component and test

--- src/app/tool-bar/tool-bar.component.spec.ts
+++ src/app/tool-bar/tool-bar.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolBarComponent } from './tool-bar.component';
 
+const baseUrl = window.location.origin;
+
 describe('ToolBarComponent', () => {
   let component: ToolBarComponent;
   let fixture: ComponentFixture<ToolBarComponent>;
@@ -19,7 +21,20 @@ describe('ToolBarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+
+	it('should render the logo and the Welcome message', () => {		
+		const compiled = fixture.nativeElement as HTMLElement;
+		const img = compiled.querySelector('.toolbar-img') as HTMLImageElement;	
+		expect(img?.src).toEqual(`${baseUrl}/assets/images/logo.png`);
+		const msg = compiled.querySelector('.toolbar-message');
+		expect(msg?.textContent).toEqual('Welcome Admin');
+		const avatar = compiled.querySelector('.toolbar-avatar') as HTMLImageElement;
+		expect(avatar?.src).toBeDefined();
+	});
+
+
 });
